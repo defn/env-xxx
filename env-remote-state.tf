620be0af1ff143315c70cdac35114a1670166a9e
Tom Bombadil <amanibhavam@destructuring.org>
2016-06-28 23:40:15 -0700

    remote state separated

---
+++ env-remote-state.tf
@@ -0,0 +1,7 @@
+data "terraform_remote_state" "env" {
+  backend = "s3"
+  config {
+    bucket = "${var.bucket_remote_state}"
+    key = "${var.bucket_remote_state}/env-${var.context_org}-${var.context_env}.tfstate"
+  }
+}
--- fogg.tf
+++ fogg.tf
@@ -1,13 +1,5 @@
 provider "aws" { }
 
-data "terraform_remote_state" "env" {
-  backend = "s3"
-  config {
-    bucket = "${var.bucket_remote_state}"
-    key = "${var.bucket_remote_state}/env-${var.context_org}-${var.context_env}.tfstate"
-  }
-}
-
 module "env" {
   source = "../module-aws-env"
 

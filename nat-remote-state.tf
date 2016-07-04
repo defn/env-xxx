4c95d114ea3988488e2faf2ac29cbf82754ae29a
Tom Bombadil <amanibhavam@destructuring.org>
2016-07-03 20:17:09 -0700

    wrong key for nat remote state

--- nat-remote-state.tf
+++ nat-remote-state.tf
@@ -2,6 +2,6 @@ data "terraform_remote_state" "nat" {
   backend = "s3"
   config {
     bucket = "${var.bucket_remote_state}"
-    key = "${var.bucket_remote_state}/nat-${var.context_org}-${var.context_env}.tfstate"
+    key = "${var.bucket_remote_state}/env-${var.context_org}-${var.context_env}-nat.tfstate"
   }
 }
